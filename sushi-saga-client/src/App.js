c8afe579b7d7ea076f15d6fc9de5b8ad1692ff2b
amitdesh <amitdesh2014@gmail.com>
2020-09-19 17:16:50 -0400

    done

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -1,17 +1,62 @@
-import React, { Component } from 'react';
-import SushiContainer from './containers/SushiContainer';
-import Table from './containers/Table';
+import React, { Component } from "react";
+import SushiContainer from "./containers/SushiContainer";
+import Table from "./containers/Table";
 
 // Endpoint!
-const API = "http://localhost:3000/sushis"
+const API = "http://localhost:3000/sushis/";
 
 class App extends Component {
+  state = {
+    sushis: [],
+    eaten: [],
+    wallet: 100,
+    display: 0,
+  };
+
+  componentDidMount() {
+    return fetch(API)
+      .then((resp) => resp.json())
+      .then((data) => {
+        this.setState(() => ({ sushis: data }));
+      });
+  }
+
+  displaySushis = () => {
+    return this.state.sushis.slice(this.state.display, this.state.display + 4);
+  };
+
+  moreSushis = (e) => {
+    let newSushis = this.state.display + 4;
+    this.setState(() => ({
+      display: newSushis,
+    }));
+  };
+
+  eatSushi = (sushi) => {
+    let newWallet = this.state.wallet - sushi.price;
+
+    if (!this.state.eaten.includes(sushi) && newWallet >= 0) {
+      this.setState((prevEaten) => ({
+        wallet: newWallet,
+        eaten: [...prevEaten, sushi],
+      }));
+    }
+  };
+
+  clickHandler = (sushiObj) => {
+    console.log("Sushi clicked", sushiObj);
+  };
 
   render() {
     return (
       <div className="app">
-        <SushiContainer />
-        <Table />
+        <SushiContainer
+          sushis={this.displaySushis()}
+          moreSushis={this.moreSushis}
+          eaten={this.state.eaten}
+          eatSushi={this.eatSushi}
+        />
+        <Table wallet={this.state.wallet} eaten={this.state.eaten} />
       </div>
     );
   }
