edaee45a1d2a6d7c178dda3a4e5cdd4363674b7c
github username <joshmrallen@gmail.com>
2020-09-20 13:18:58 -0400

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import AddMoney from './components/AddMoney';
 import SushiContainer from './containers/SushiContainer';
 import Table from './containers/Table';
 
@@ -7,11 +8,69 @@ const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+  state = {
+    sushi: [],
+    dishes: [],
+    wallet: 100
+  }
+
+  componentDidMount(){
+    fetch(API)
+      .then(res=>res.json())
+      .then(sushiArray => {
+        let newArray = sushiArray.map(sushi => ({
+          ...sushi,
+          eaten: false
+        }))
+        this.setState(()=>({
+          sushi: newArray
+        }))
+        console.log(`%c Array received with ${newArray.length} sushi!`, 'color: green')
+        // console.log(newArray)
+      })
+  }
+
+  eatHandler = (sushiObj) => {
+
+    let newWallet = this.state.wallet
+    newWallet -= sushiObj.price
+
+    if(newWallet >= 0){
+      let newArray = this.state.sushi
+      let foundObj = newArray.find(sushi => sushi.id === sushiObj.id)
+      foundObj.eaten = true
+  
+      let dishArray = this.state.dishes
+      dishArray.push(sushiObj)
+  
+      this.setState(()=>({
+        sushi: newArray,
+        dishes: dishArray,
+        wallet: newWallet
+      }))
+    } else {
+      console.log("%c You've reached your budget and can't spend any more!", 'color: red')
+    }
+
+  }
+
+  submitHandler = (amount) => {
+    console.log(amount)
+    this.setState((previousState)=>{
+      let newAmount = previousState.wallet + amount
+      console.log(newAmount)
+      return {
+        wallet: newAmount
+      }
+    })
+  }
+
   render() {
     return (
       <div className="app">
-        <SushiContainer />
-        <Table />
+        <AddMoney submitHandler={this.submitHandler} />
+        <SushiContainer sushi={this.state.sushi} eatHandler={this.eatHandler} />
+        <Table dishes={this.state.dishes} wallet={this.state.wallet} />
       </div>
     );
   }
--- sushi-saga-client/src/containers/Table.js
+++ sushi-saga-client/src/containers/Table.js
@@ -3,15 +3,26 @@ import React, { Fragment } from 'react'
 const Table = (props) => {
 
   const renderPlates = (array) => {
+    console.log(props.dishes)
     return array.map((x, index) => {
-      return <div className="empty-plate" style={{ top: -7 * index }}/>
+      return <div key={index} className="empty-plate" style={{ top: -7 * index }}/>
     })
   }
+
+  // const budget = () => {
+  //   let wallet = props.wallet
+  //   props.dishes.forEach(sushi => {
+  //     if(wallet - sushi.price >= 0){
+  //       wallet -= sushi.price
+  //     }
+  //   })
+  //   return wallet
+  // }
 
   return (
     <Fragment>
       <h1 className="remaining">
-        You have: ${ /* Give me how much money I have left */ } remaining!
+        You have: ${props.wallet} remaining!
       </h1>
       <div className="table">
         <div className="stack">
@@ -21,7 +32,7 @@ const Table = (props) => {
                and renders an empty plate
                for every element in the array
             */
-            renderPlates([])
+            renderPlates(props.dishes)
           }
         </div>
       </div>
